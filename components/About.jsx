8f406c1aaaaad889b27939285e947f2467246d80
Nicolas Lévêque <nicoleveque@outlook.com>
2022-01-14 14:48:43 +0100

    install framer motion for animations, animate about components

--- components/About.jsx
+++ components/About.jsx
@@ -1,15 +1,30 @@
-import Image from 'next/image'
-
-import photoProfil from '../public/data/nicolas2.jpeg'
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import photoProfil from '../public/data/nicolas2.jpeg';
 
 export default function About() {
-  
-  return (
+	return (
 		<section id="about" className="max-w-screen-lg my-5">
-			<div className="mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-				<div
-					className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center"
-				>
+			<motion.div
+				initial="hidden"
+				animate="visible"
+				variants={{
+					hidden: {
+						scale: 0.8,
+						opacity: 0,
+					},
+					visible: {
+						scale: 1,
+						opacity: 1,
+						transition: {
+							delay: 0.2,
+							duration: 1,
+						},
+					},
+				}}
+				className="mx-auto flex px-10 py-20 md:flex-row flex-col items-center"
+			>
+				<div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
 					<h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
 						Bonjour, <br />
 						je m&apos;appelle Nicolas
@@ -37,19 +52,17 @@ export default function About() {
 						</a>
 					</div>
 				</div>
-				<div
-					className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 py-20 flex justify-center"
-				>
+				<div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 py-20 flex justify-center">
 					<Image
-            width={350}
-            height={350}
-            objectFit="cover"
-            src={photoProfil}
-            alt="photo de profil"
-            className="object-cover object-center rounded-full "
-          />
+						width={350}
+						height={350}
+						objectFit="cover"
+						src={photoProfil}
+						alt="photo de profil"
+						className="object-cover object-center rounded-full "
+					/>
 				</div>
-			</div>
+			</motion.div>
 		</section>
 	);
 }
